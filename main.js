c202fb7443175c932b86e5dee7a5db90b7fe30be
dareunk <happyeunnn11@gmail.com>
2023-05-12 05:13:48 +0000

    commit

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const port =80,
 	express = require("express"),
 	app = express(),
 	homeController = require("./controllers/homeController");
-app.use(homeController.mainScreen);
 app.use(express.static("public"));
+app.use(homeController.mainScreen);
 app.listen(port);
 console.log(`Listening on port ${port}`);
 
